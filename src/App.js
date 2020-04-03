6f8ceff522e3a14c3e5d7f1a6dc51f773cc8741e
julie <julie@julies-MBP.attlocal.net>
2020-04-03 08:28:19 -0700

    add home to app

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
       <Switch>
         <Route exact path="/" component={ Home } />
         <Route path="/search" component={ Search } />
