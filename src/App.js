618414c6f6745bb9fbd31f9f3dca07ad3f820a9a
julie <julie@julies-MacBook-Pro.local>
2020-04-05 14:32:16 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -1,35 +1,18 @@
 import React from "react"
-<<<<<<< HEAD
 import { Switch, Route } from "react-router-dom"
-=======
-import { Switch, Route, Redirect } from "react-router-dom"
->>>>>>> 4bca6c67de9c99cd62eb5949b0e94f7345c9a7dd
 import Search from "./Components/Search/Search"
 import Home from "./Components/Home/Home"
 import Header from "./Components/Header"
 import "./App.css"
 
-<<<<<<< HEAD
 export default function App() {
-=======
-function App() {
->>>>>>> 4bca6c67de9c99cd62eb5949b0e94f7345c9a7dd
   return (
     <div className="App">
       <Header />
       <Switch>
         <Route exact path="/recipe-app" component={ Home } />
         <Route path="/search" component={ Search } />
-<<<<<<< HEAD
-=======
-        <Redirect from="/" to="/recipe-app" />
->>>>>>> 4bca6c67de9c99cd62eb5949b0e94f7345c9a7dd
       </Switch>
     </div>
   )
 }
-<<<<<<< HEAD
-=======
-
-export default App;
->>>>>>> 4bca6c67de9c99cd62eb5949b0e94f7345c9a7dd
--- src/Components/Recipes/Recipes.js
+++ src/Components/Recipes/Recipes.js
@@ -7,11 +7,7 @@ export default function Recipes(props){
     const recipeList = props.recipes.hits.map((card, i) => {
         return (
             <div key={i} style={{flex: "22%"}}>
-<<<<<<< HEAD
-                <Card className="recipe-search">
-=======
                 <Card className="recipe-search" style={{ width: "18rem"}}>
->>>>>>> 4bca6c67de9c99cd62eb5949b0e94f7345c9a7dd
                     <Card.Link target="_blank" href={card.recipe.shareAs}>
                         <Card.Img variant="top" alt="Food Image" src={card.recipe.image} />
                     </Card.Link>
@@ -19,10 +15,7 @@ export default function Recipes(props){
                         <Card.Title>
                                 {card.recipe.label}
                         </Card.Title>
-<<<<<<< HEAD
-=======
                         <Card.Text>{card.recipe.mealType}</Card.Text>
->>>>>>> 4bca6c67de9c99cd62eb5949b0e94f7345c9a7dd
                         <ul>
                             <li>{card.recipe.ingredientLines[0]}</li>
                             <li>{card.recipe.ingredientLines[1]}</li>
