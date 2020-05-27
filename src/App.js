d69c47ca52ebfa71acf4bbcdfb615e45a3a513e5
julie <julie@julies-MacBook-Pro.local>
2020-05-27 16:49:21 -0700

    remove header

--- src/App.js
+++ src/App.js
@@ -1,41 +1,40 @@
 import React, { useState, createContext } from "react"
 import { Switch, Route } from "react-router-dom"
 import Home from "./Components/Home/Home"
-import Header from "./Components/Header"
 import Register from "./Authentication/Register"
 import Login from "./Authentication/Login"
 import CreateRecipe from "./Authentication/CreateRecipe"
 import UserRecipes from "./Components/UserRecipes/UserRecipes"
+import Footer from "./Components/Footer/Footer"
 import "./App.css"
 
 export default function App() {
   const [user, setUser] = useState([])
-  const [recipes, setRecipes] = useState([])
+  const [newRecipes, setNewRecipes] = useState([])
   const [userRecipes, setUserRecipes] = useState([])
   const [loggedIn, setLoggedIn] = useState(false)
 
-
   return (
     <div className="App">
-      <Header />
       <RecipeContext.Provider value={{
         user,
         setUser,
-        recipes,
-        setRecipes,
+        newRecipes,
+        setNewRecipes,
         userRecipes,
         setUserRecipes,
         loggedIn,
         setLoggedIn
       }}>
         <Switch>
-          {/* <Route exact path="/" component={ Home } /> */}
+          <Route exact path="/" component={ Home } />
           <Route path="/register" component={ Register } />
           <Route path="/login" component={ Login } />
           <Route path="/createrecipe" component={ CreateRecipe } />
           <Route path="/recipes" component={ UserRecipes } />
         </Switch>
       </RecipeContext.Provider>
+      <Footer />
     </div>
   )
 }
