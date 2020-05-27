d9aa4289ae4381866d948495757502794c8a3d7a
julie <julie@julies-MacBook-Pro.local>
2020-05-27 16:50:42 -0700

    style register component

--- src/Authentication/Register.js
+++ src/Authentication/Register.js
@@ -1,11 +1,13 @@
 import React, { useState, useContext } from "react"
+import { useMediaQuery } from "react-responsive"
 import { registerNewUser } from "../Services/api-helper"
 import { Form } from "react-bootstrap"
 import { RecipeContext } from "../App"
+import "./Register.scss"
 
 export default function Register(props){
+    const isDesktop = useMediaQuery({query: "(min-device-width: 1020px)"})
     const recipeContext = useContext(RecipeContext)
-    console.log(recipeContext)
     const [newUser, setNewUser] = useState({
         first_name: "",
         last_name: "",
@@ -34,57 +36,112 @@ export default function Register(props){
 
     return (
         <>
-        <Form onSubmit={ handleSubmit }>
-            <div className="label">
-                <label>First name:</label>
-            </div>
-            <Form.Control 
-                name="first_name" 
-                type="text" 
-                value={newUser.first_name} 
-                onChange={ handleChange }  
-            />
-    
-            <div className="label">
-                <label>Last name:</label>
-            </div>
-            <Form.Control 
-                name="last_name" 
-                value={newUser.last_name} 
-                onChange={ handleChange } 
-            />
+            {
+                isDesktop ?
+                    <Form style={{margin: "13% auto"}} className="register" onSubmit={ handleSubmit }>
+                        <div className="label">
+                            <label>First name:</label>
+                        </div>
+                        <Form.Control 
+                            name="first_name" 
+                            type="text" 
+                            value={newUser.first_name} 
+                            onChange={ handleChange }  
+                        />
+                
+                        <div className="label">
+                            <label>Last name:</label>
+                        </div>
+                        <Form.Control 
+                            name="last_name" 
+                            value={newUser.last_name} 
+                            onChange={ handleChange } 
+                        />
 
-            <div className="label">
-                <label>Username:</label>
-            </div>
-            <Form.Control 
-                name="username" 
-                value={newUser.username} 
-                onChange={ handleChange }  
-            />
+                        <div className="label">
+                            <label>Username:</label>
+                        </div>
+                        <Form.Control 
+                            name="username" 
+                            value={newUser.username} 
+                            onChange={ handleChange }  
+                        />
 
-            <div className="label">
-                <label>Email:</label>
-            </div>
-            <Form.Control 
-                name="email" 
-                value={newUser.email} 
-                onChange={ handleChange } 
-            />
+                        <div className="label">
+                            <label>Email:</label>
+                        </div>
+                        <Form.Control 
+                            name="email" 
+                            value={newUser.email} 
+                            onChange={ handleChange } 
+                        />
 
-            <div className="label">
-                <label>Password:</label>
-            </div>
-            <Form.Control 
-                name="password" 
-                type="password"
-                value={newUser.password} 
-                onChange={ handleChange } 
-            />
+                        <div className="label">
+                            <label>Password:</label>
+                        </div>
+                        <Form.Control 
+                            name="password" 
+                            type="password"
+                            value={newUser.password} 
+                            onChange={ handleChange } 
+                        />
 
-            <Form.Control className="register-submit" type="Submit" value="Submit" />
+                        <input className="register-submit" type="Submit" value="Submit" />
 
-        </Form>
+                    </Form> :
+
+                    <Form className="register" onSubmit={ handleSubmit }>
+                        <div className="label">
+                            <label>First name:</label>
+                        </div>
+                        <Form.Control 
+                            name="first_name" 
+                            type="text" 
+                            value={newUser.first_name} 
+                            onChange={ handleChange }  
+                        />
+                
+                        <div className="label">
+                            <label>Last name:</label>
+                        </div>
+                        <Form.Control 
+                            name="last_name" 
+                            value={newUser.last_name} 
+                            onChange={ handleChange } 
+                        />
+
+                        <div className="label">
+                            <label>Username:</label>
+                        </div>
+                        <Form.Control 
+                            name="username" 
+                            value={newUser.username} 
+                            onChange={ handleChange }  
+                        />
+
+                        <div className="label">
+                            <label>Email:</label>
+                        </div>
+                        <Form.Control 
+                            name="email" 
+                            value={newUser.email} 
+                            onChange={ handleChange } 
+                        />
+
+                        <div className="label">
+                            <label>Password:</label>
+                        </div>
+                        <Form.Control 
+                            name="password" 
+                            type="password"
+                            value={newUser.password} 
+                            onChange={ handleChange } 
+                        />
+
+                        <input className="register-submit" type="Submit" value="Submit" />
+
+                    </Form>
+            }
         </>
     )
 }
