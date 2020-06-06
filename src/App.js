7024d034dca7d1ce5c5d61cd64da5716cb5e7cdb
julie <julie@julies-MacBook-Pro.local>
2020-06-06 00:36:11 -0700

    styled app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import React, { useState, useEffect, createContext } from "react"
+import React, { useState, useLayoutEffect } from "react"
 import { Switch, Route } from "react-router-dom"
-import { useMediaQuery } from "react-responsive"
 import Header from "./Components/Header"
-// import Home from "./Components/Home/Home"
 import Recipes from "./Components/Recipes/Recipes"
 import RecipeDetails from "./Components/RecipeDetails/RecipeDetails"
 import Footer from "./Components/Footer/Footer"
 import "./App.css"
 
 export default function App() {
-  const isMobile = useMediaQuery({query: "(min-device-width: 500px)"})
   const [recipes, setRecipes] = useState({})
   const [foodType, setFoodType] = useState("")
   const [search, setSearch] = useState("steak")
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     const callApi = async () => {
       const apiKey = `1165067289152cf22ecf26a6bb0bcd41`
       const appId = `6227bd9a` 
@@ -40,14 +37,10 @@ export default function App() {
   return (
     <div className="App">
       <Header recipes={recipes} foodType={foodType} handleSubmit={handleSubmit} handleChange={handleChange} />
-            {/* {!isMobile ? " " : <Images />} */}
-            <h1 className="hungry-title">Hungry?</h1>
-            <hr />
-        <Switch>
-          <Route exact path="/" component={() => recipes.hits ? (<Recipes recipes={recipes} />) : " " } />
-          {/* <Route exact path="/" component={() => <Home recipes={recipes} /> } /> */}
-          <Route path="/recipe/:ingredients" component={ RecipeDetails } />
-        </Switch>
+      <Switch>
+        <Route exact path="/" component={() => recipes.hits ? (<Recipes recipes={recipes} />) : " " } />
+        <Route path="/:ingredients" component={ RecipeDetails } />
+      </Switch>
       <Footer />
     </div>
   )
